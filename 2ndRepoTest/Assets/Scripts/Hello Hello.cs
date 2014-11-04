860e4858813d904101b9ff573671caf624175db5
Louis Guillemin <louisguillemin@msn.com>
2014-11-04 19:28:43 +0100

    rajout de 2 .cs + espoir que ca veuille bien me laisser envoyer ou recevoir

---
+++ 2ndRepoTest/Assets/Scripts/Hello Hello.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloHello : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello Hell");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
